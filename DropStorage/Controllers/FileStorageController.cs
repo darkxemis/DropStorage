91dd06e0eedbcb80d61603c9ee01ed4bcd18204d
Jose miguel <josemigueljaimezdiaz@gmail.com>
2022-11-30 15:40:16 +0100

    Terminado descarga de ficheros

--- DropStorage/Controllers/FileStorageController.cs
+++ DropStorage/Controllers/FileStorageController.cs
@@ -19,7 +19,7 @@ namespace DropStorage.Controllers
 
         [AllowAnonymous]
         [Route("api/auth/getallfiles")]
-        [HttpPost]
+        [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<List<FileStorageDTO>> GetAllFilesByUserId(Guid idUser)
@@ -36,18 +36,15 @@ namespace DropStorage.Controllers
         {
             List<FileStorage> fileStorageList = await _fileStorageService.GetInfoFiles(ids);
 
-            var zipName = $"TestFiles-{DateTime.Now.ToString("yyyy_MM_dd-HH_mm_ss")}.zip";
+            var zipName = $"{DateTime.Now.ToString("yyyy_MM_dd-HH_mm_ss")}.zip";
             using (MemoryStream ms = new MemoryStream())
             {
-                //required: using System.IO.Compression;
                 using (var zip = new ZipArchive(ms, ZipArchiveMode.Create, true))
                 {
                     foreach (FileStorage fileStorage in fileStorageList)
                     {
-                        //QUery the Products table and get all image content
                         var entry = zip.CreateEntry(fileStorage.Name);
                         using (var entryStream = entry.Open())
-                        //using (var fileStream = System.IO.File.OpenRead("C:\\Users\\josemi\\Desktop\\Nueva carpeta\\Prueba.txt"))
                         using (var fileStream = System.IO.File.OpenRead(fileStorage.Url))
                         {
                             await fileStream.CopyToAsync(entryStream);
